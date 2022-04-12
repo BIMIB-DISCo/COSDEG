513069b136831e9cd9d3d6e47efa0ad29d1ed6c9
Gianluca Ascolani <64789019+gian-asco@users.noreply.github.com>
2022-04-12 11:47:30 +0200

    Update cosdeg.R

--- cosdeg.R
+++ cosdeg.R
@@ -7,7 +7,8 @@ for (i in file_list) {
   download.file(url = paste0(webpath, i), destfile = paste0("./data/raw/", i))
 }
 
-devtools::install_github("https://github.com/chris-mcginnis-ucsf/DoubletFinder", upgrade = F)
+install_github("https://github.com/chris-mcginnis-ucsf/DoubletFinder", upgrade = F)
+
 
 
 
@@ -19,8 +20,8 @@ suppressMessages(require(DoubletFinder))
 dataset_folders <- list.dirs(path = "data/", full.names = F, recursive = F)
 dataset_folders <- dataset_folders[-5] # remove raw folder
 
+summary_qc <- data.frame()
 
-#load data
 s_data <- list()
 data.filt <- list()
 s_data_dim <- list()
@@ -30,16 +31,19 @@ for (d in dataset_folders) {
   print(data_dir)
   tmp_expression <- Seurat::ReadMtx(file.path(data_dir,"matrix.mtx.gz"), features = file.path(data_dir,"features.tsv.gz"), cells = file.path(data_dir,"barcodes.tsv.gz"))
   s_data[[d]] <- CreateSeuratObject(counts = tmp_expression, project = d)
-  s_data_dim[[d]] <- dim(s_data[[d]])
+  s_data_dim[[d]] <- list(dim(s_data[[d]]))
+  summary_qc["preQC",d] <- s_data_dim[d]
   
+  # Way1: Doing it using Seurat function
   s_data[[d]] <- PercentageFeatureSet(s_data[[d]], "^MT-", col.name = "percent_mito")
   s_data[[d]] <- PercentageFeatureSet(s_data[[d]], "^RP[SL]", col.name = "percent_ribo")
+  # Percentage hemoglobin genes - includes all genes starting with HB except HBP.
   s_data[[d]] <- PercentageFeatureSet(s_data[[d]], "^HB[^(P)]", col.name = "percent_hb")
   s_data[[d]] <- PercentageFeatureSet(s_data[[d]], "PECAM1|PF4", col.name = "percent_plat")
   
 }
 
-
+summary_qc
 
 # Merge datasets into one single seurat object
 alldata <- merge(s_data[[1]], s_data[-1], add.cell.ids = names(s_data))
@@ -51,20 +55,19 @@ VlnPlot(alldata, group.by = "orig.ident", features = feats, pt.size = 0.1, ncol 
 
 
 
-summary_qc <- data.frame(s_data_dim)
-summary_qc
 
-#selecting expressed genes
-selected_f0 <- list()
+
+selected_f <- list()
 for (d in dataset_folders) {
-  selected_f0[[d]] <- rownames(s_data[[d]])[Matrix::rowSums(s_data[[d]]) > 0]
-  data.filt[[d]] <- subset(s_data[[d]], features = selected_f0[[d]])
-  s_data_dim[[d]] <- length(selected_f0[[d]])
-  #s_data_dim[[d]] <- dim(data.filt[[d]])
+  selected_f[[d]] <- rownames(s_data[[d]])[Matrix::rowSums(s_data[[d]]) > 0]
+  data.filt[[d]] <- subset(s_data[[d]], features = selected_f[[d]])
+  #s_data_dim[[d]] <- length(selected_f[[d]])
+  s_data_dim[[d]] <- list(dim(data.filt[[d]]))
+  summary_qc["zero_genes",d] <- s_data_dim[d]
+  
   print(dim(data.filt[[d]]))
 }
 
-summary_qc[3,]=s_data_dim
 summary_qc
 
 
@@ -74,35 +77,37 @@ summary_qc
 selected_c <- list()
 perc_zeros <- 0.90
 for (d in dataset_folders) {
-  selected_c90[[d]] <- colnames(data.filt[[d]])[data.filt[[d]]$nFeature_RNA/dim(data.filt[[d]])[1]>=1-perc_zeros]
-  data.filt[[d]] <- subset(data.filt[[d]], cells = selected_c90[[d]])
-  s_data_dim[[d]] <- length(selected_c90[[d]])
-  #s_data_dim[[d]] <- dim(data.filt[[d]])
+  selected_c[[d]] <- colnames(data.filt[[d]])[data.filt[[d]]$nFeature_RNA/dim(data.filt[[d]])[1]>=1-perc_zeros]
+  data.filt[[d]] <- subset(data.filt[[d]], cells = selected_c[[d]])
+  #s_data_dim[[d]] <- length(selected_c[[d]])
+  s_data_dim[[d]] <- list(dim(data.filt[[d]]))
+  summary_qc["empty_cells",d] <- s_data_dim[d]
+  
   print(dim(data.filt[[d]]))
 }
 
-summary_qc[4,]=s_data_dim
 summary_qc
 
-#selecting cells with less than 1 std from the mean of mitochondrial content  
 perc_mitoc <- list()
 for (d in dataset_folders) {
+  # Way1: Doing it using Seurat function
   data.filt[[d]] <- PercentageFeatureSet(data.filt[[d]], "^MT-", col.name = "percent_mito")
   data.filt[[d]] <- PercentageFeatureSet(data.filt[[d]], "^RP[SL]", col.name = "percent_ribo")
+  # Percentage hemoglobin genes - includes all genes starting with HB except HBP.
   data.filt[[d]] <- PercentageFeatureSet(data.filt[[d]], "^HB[^(P)]", col.name = "percent_hb")
   data.filt[[d]] <- PercentageFeatureSet(data.filt[[d]], "PECAM1|PF4", col.name = "percent_plat")
   
   perc_mitoc[[d]] <- mean(data.filt[[d]]$percent_mito/100.) + sd(data.filt[[d]]$percent_mito/100.)
   
-  selected_cMT[[d]] <- colnames(data.filt[[d]])[data.filt[[d]]$percent_mito/100 <= perc_mitoc[[d]]]
+  selected_c[[d]] <- colnames(data.filt[[d]])[data.filt[[d]]$percent_mito/100 <= perc_mitoc[[d]]]
   
-  data.filt[[d]] <- subset(data.filt[[d]], cells = selected_cMT[[d]])
-  s_data_dim[[d]] <- length(selected_cMT[[d]])
-  #s_data_dim[[d]] <- dim(data.filt[[d]])
+  data.filt[[d]] <- subset(data.filt[[d]], cells = selected_c[[d]])
+  #s_data_dim[[d]] <- length(selected_c[[d]])
+  s_data_dim[[d]] <- list(dim(data.filt[[d]]))
+  summary_qc["mitochodrial",d] <- s_data_dim[d]
   print(dim(data.filt[[d]]))
 }
 
-summary_qc[6,]=s_data_dim
 summary_qc
 
 for (d in dataset_folders) {
@@ -117,7 +122,7 @@ for (d in dataset_folders) {
           col = (scales::hue_pal())(20)[20:1], horizontal = TRUE)
 }
 
-# Merge filtered datasets into one single seurat object
+# Merge datasets into one single seurat object
 alldata <- merge(data.filt[[1]], data.filt[-1], add.cell.ids = names(data.filt))
 
 feats <- c("nFeature_RNA", "nCount_RNA", "percent_mito", "percent_ribo", "percent_hb")
@@ -144,9 +149,13 @@ for (d in dataset_folders) {
 
 
   # Can run parameter optimization with paramSweep, but skip for now.
-  # sweep.res <- paramSweep_v3(data.filt) sweep.stats <-
-  # summarizeSweep(sweep.res, GT = FALSE) bcmvn <- find.pK(sweep.stats)
-  # barplot(bcmvn$BCmetric, names.arg = bcmvn$pK, las=2)
+  sweep.res[[d]] <- paramSweep_v3(data.filt[[d]]) 
+  sweep.stats[[d]] <- summarizeSweep(sweep.res[[d]], GT = FALSE) 
+  bcmvn[[d]] <- find.pK(sweep.stats[[d]])
+  barplot(bcmvn[[d]]$BCmetric, names.arg = bcmvn[[d]]$pK, las=2)
+}
+
+for (d in dataset_folders) {
   # define the expected number of doublet cellscells.
   nExp <- round(ncol(data.filt[[d]]) * 0.16)  # expect 4% doublets
   data.filt[[d]] <- doubletFinder_v3(data.filt[[d]], pN = 0.25, pK = 0.09, nExp = nExp, PCs = 1:10)
@@ -161,7 +170,13 @@ for (d in dataset_folders) {
                      DimPlot(data.filt[[d]], group.by = DF.name) + NoAxes())
   
   VlnPlot(data.filt[[d]], features = "nFeature_RNA", group.by = DF.name, pt.size = 0.1)
+  
+  s_data_dim[[d]] <- list(dim(data.filt[[d]]))
+  summary_qc["doublets",d] <- s_data_dim[d]
 }
+
+
+summary_qc
 
 #########
 
@@ -222,9 +237,5 @@ selected_c <- colnames(alldata)[alldata$nFeature_RNA/dim(alldata)[1]>=1-perc_zer
 
 data.filt <- subset(alldata, features = selected_f, cells = selected_c)
 dim(data.filt)
-
-
-
-
 
 
