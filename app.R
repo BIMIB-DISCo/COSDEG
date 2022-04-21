48ced261da0b3e6781efdb9bb9c389219a76899c
gian asco <gianluca.ascolani@unimib.it>
2022-04-21 12:52:02 +0200

    dataset name
    function identifing possible dataset neme

--- app.R
+++ app.R
@@ -77,6 +77,8 @@ body <- dashboardBody(
 
 # check is a dataset folder
 check_dataset_dir <- function(folder) {
+  if (is.null(folder))
+    return(NULL)
   if(dir.exists(folder)) {
     mtx <- list.files(path = folder, 
                pattern = ".*\\.mtx|.*\\.mtx\\.gz|.*\\.h5", 
@@ -84,28 +86,99 @@ check_dataset_dir <- function(folder) {
                no.. = TRUE, recursive = TRUE
               )
     if(length(mtx)>1) {
+      print("Found multiple datasets")
+      mtx_files <- lapply(mtx, basename)
+      test_fun <- function(x){stringi::stri_detect_regex(mtx_files, pattern=x)}
+      test_string <- c(".h5")
+      res_test <- sapply(test_string, test_fun)
+      if (any(res_test)) { # if h5 add files and check rest
+        #print("test")
+        #print(mtx[res_test])
+        #print("anti test")
+        #print(mtx[!res_test])
+        #browser()
+        return(c(mtx[res_test], check_multidatasets_dir( dirname(mtx[!res_test])) ) )        
+      }
       return(check_multidatasets_dir(folder))
     }
     else if (length(mtx) == 0) {
+      print("No datasets found")
       return(NULL)
     }
     else {
+      print("Found 1 dataset")
       mtx_dir <- dirname(mtx[[1]])
-      mtx_files <- list.files(path = folder, 
+      mtx_files <- list.files(path = mtx_dir, 
                         pattern = ".*\\.mtx|.*\\.mtx\\.gz|.*\\.h5|.*\\.tsv|.*\\.tsv\\.gz", 
                         full.names = TRUE, include.dirs = FALSE, 
                         no.. = TRUE, recursive = TRUE
       )
-      mtx_files <- lapply(mtx_files, basename)
-      
+      mtx_basefiles <- lapply(mtx_files, basename)
+      test_fun <- function(x){any(stringi::stri_detect_regex(mtx_basefiles, pattern=x))}
+      test_string <- c("barcodes.tsv","features.tsv","matrix.mtx")
+      if (all(sapply(test_string, test_fun))) {
+        return(mtx_dir)
+      } 
+      test_string <- c(".h5")
+      if (all(sapply(test_string, test_fun))) {
+        return(mtx_files)
+        #return(mtx_dir)        
+      }
+      return(NULL)
     }
-      
-    
   }
+  return(NULL)
 }
 
 # check is multi-datasets folder
-
+check_multidatasets_dir <- function(folder) {
+  if (is.null(folder) || length(folder) == 0)
+    return(NULL)
+  
+  if(length(folder)>1)
+    return(lapply(folder, check_multidatasets_dir))
+  #print("FFF")
+  #print(folder)
+  if(dir.exists(folder)) {
+    mtx <- list.files(path = folder, 
+                      pattern = ".*\\.mtx|.*\\.mtx\\.gz|.*\\.h5", 
+                      full.names = TRUE, include.dirs = FALSE, 
+                      no.. = TRUE, recursive = TRUE
+    )
+    
+    if(length(mtx)==0)
+      return(NULL)
+    
+    #print("AAA")
+    #print(mtx)
+    dirs <- list()
+    file_dirs <- dirname(mtx)
+    file_dirs <- unique(file_dirs)
+    for (d in file_dirs) {
+      #print("DDD")
+      #print(d)
+      dirs <- c(dirs, check_dataset_dir(d))
+    }
+    #browser()
+    if(length(dirs)>0) {
+      return(dirs)
+    }
+    return(NULL)
+  }
+}
+ 
+meaningful_name_path <- function(dataset_dir_list) {
+  names_ <- xfun::sans_ext(dataset_dir_list)
+  
+  names_ <- stringr::str_split(unlist(names_),pattern = "/")
+  dup_names_ <- unique(unlist(names_)[duplicated(unlist(names_))])
+  unique(unlist(stringr::str_split(unlist(dataset_dir_list),pattern = "/")))
+  known <- "data|dataset|raw|filtered|barcode|feature|matrix|cazzo"
+  pat <- stringi::stri_replace_all_fixed(paste0(c(dup_names_,known),collapse = "|"), ".", "\\.")  
+  probable_name <- lapply(names_,function(x){x[!stringi::stri_detect_regex(x, pattern = pat)]})
+  return(probable_name)
+}
+ 
 # Define UI for data upload app ----
 ui <- fluidPage(
   dashboardPage(
