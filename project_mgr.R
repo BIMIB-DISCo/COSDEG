54d601aec31bb8f9803ead1c8b1ec18ba69b441b
gian asco <gianluca.ascolani@unimib.it>
2022-04-26 12:26:17 +0200

    project manager

---
+++ project_mgr.R
@@ -0,0 +1,185 @@
+cosdeg_version <- "v0.8.0"
+
+projects <- list()
+
+
+###
+
+normalize_proj_name <- function(project_name) {
+  proj_names <- names(projects)
+  tmp_project_name_ <- stringi::stri_replace_all_charclass(project_name, '[,.;:.|!?$\\ /\\|*%\\&()@#]', '_', merge=TRUE)
+  tmp_project_name <- tmp_project_name_
+  for (counter in  1:10) {
+    if (any(stringi::stri_detect_fixed(proj_names, pattern = tmp_project_name))) {
+      tmp_project_name <- paste0(tmp_project_name_, "_", counter)
+    }
+    else
+      return(tmp_project_name)
+  }
+  if (counter == 10)
+    return(NULL)
+}
+
+
+normalize_tab_name <- function(project_name) {
+  #proj_names <- c()
+  tab_names <- c()
+  #tab_ids <- c()
+  for (proj in projects) {
+    #proj_names <- c(proj_names, proj[["project_name"]])
+    tab_names <- c(tab_names, proj[["tab_name"]])
+    #tab_ids <- c(tab_ids, proj[["tab_id"]])
+  }
+  
+  tmp_project_name_ <- project_name
+  tmp_project_name <- tmp_project_name_
+  for (counter in  1:10) {
+    if (any(stringi::stri_detect_fixed(tab_names, pattern = tmp_project_name))) {
+      tmp_project_name <- paste0(tmp_project_name_, " (", counter,")")
+    }
+    else
+      return(tmp_project_name)
+  }
+  if (counter == 10)
+    return(NULL)
+}
+
+
+
+normalize_tab_id <- function(project_name) {
+  tab_ids <- c()
+  for (proj in projects) {
+    tab_ids <- c(tab_ids, proj[["tab_id"]])
+  }
+  
+  tmp_project_name_ <- stringi::stri_replace_all_charclass(project_name, '[,.;:.|!?$\\ /\\|*%\\&()@#]', '_', merge=TRUE)
+  tmp_project_name <- tmp_project_name_
+  
+  for (counter in  1:10) {
+    if (any(stringi::stri_detect_fixed(tab_ids, pattern = tmp_project_name))) {
+      tmp_project_name <- paste0(tmp_project_name_, "_", counter)
+    }
+    else
+      return(tmp_project_name)
+  }
+  if (counter == 10)
+    return(NULL)
+}
+
+
+
+create_proj <- function(project_name, selected_seurat_objs = NULL ) {
+  #browser()
+  tmp_ <- normalize_proj_name(project_name)
+  if (!is.null(tmp_))
+    project_id <- tmp_
+  else {
+    # rise warning
+  }
+  
+  project <- list()
+  
+  project[["COSDEG"]] <- paste("COSDEG", cosdeg_version)
+  project[["project_name"]] <- project_name
+  
+  project[["file_name"]] <- NULL
+  
+  tmp_ <- normalize_tab_name(project_name)
+  if (!is.null(tmp_))
+    project[["tab_name"]] <- tmp_
+  else {
+    # rise warning
+  }
+  
+  
+  tmp_ <- normalize_tab_id(project_id)
+  if (!is.null(tmp_))
+    project[["tab_id"]] <- tmp_
+  else {
+    # rise warning
+  }
+  
+  project[["modified"]] <- TRUE
+  
+  project[["seurat_objs"]] <- selected_seurat_objs  #copy
+  
+  projects[[project_id]] <<- project
+}
+
+
+
+save_project <- function(project_id, file_name) {
+  browser()
+  project <- projects[[project_id]]
+  COSDEG <- project[["COSDEG"]]
+  save(project, COSDEG , precheck = TRUE, file = file_name)
+}
+
+
+loadRData <- function(filename, varname=NULL){
+  #loads an RData file, and returns it
+  load(filename)
+  if (is.null(varname))
+    return(ls()[(ls() != "filename") & (ls() != "varname") ])
+  else
+    return(get(ls()[ls() == varname]))
+}
+
+load_project <- function(file_name, projects) {
+  browser()
+  load(file_name)
+  if (!any(ls() == "COSDEG"))
+    return(FALSE)
+  
+  tmp_ <- normalize_proj_name(project_name)
+  if (!is.null(tmp_))
+    project_id <- tmp_
+  else {
+    # rise warning
+  }
+  
+  #project <- list()
+  
+  #project[["COSDEG"]] <- COSDEG
+  #project[["project_name"]] <- project_name
+  
+  project[["file_name"]] <- file_name
+  
+  tmp_ <- normalize_tab_name(project_name)
+  if (!is.null(tmp_))
+    project[["tab_name"]] <- tmp_
+  else {
+    # rise warning
+  }
+  
+  
+  tmp_ <- normalize_tab_id(project_id)
+  if (!is.null(tmp_))
+    project[["tab_id"]] <- tmp_
+  else {
+    # rise warning
+  }
+  
+  project[["modified"]] <- FALSE
+  
+  #project[["seurat_objs"]] <- seurat_objs  #copy
+  
+  projects[[project_id]] <<- project
+  
+}
+
+
+create_proj_gui <- function (project_name, selected_seurat_objs = NULL) {
+  create_proj(project_name, selected_seurat_objs)
+  #create_tab
+  #activate_tab
+}
+
+
+project_name <- "dati 1"
+create_proj(project_name = project_name)
+create_proj(project_name = project_name)
+
+project_id <- names(projects)[[2]]
+save_project(project_id = project_id, file_name = "./test.Rdata")
+load_project(file_name = "./test.Rdata", projects = projects)
